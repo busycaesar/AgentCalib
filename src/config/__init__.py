67bff1b13cddc52a1bba5b82fc5ae31d9e79f138
Dev J. Shah <dev-jshah@outlook.com>
2026-07-15 07:11:26 -0400

    feat: add package facades for config, utils, and core

---
+++ src/config/__init__.py
@@ -0,0 +1,4 @@
+from .client import client
+from .paths import SKILLS_DIR
+from .messages import messages
+from .banner import BANNER, WELCOME_MESSAGE
---
+++ src/core/__init__.py
@@ -0,0 +1,3 @@
+from .agent import agent_run
+from .slash_command import get_skill_content
+from .build_initial_message import messages
